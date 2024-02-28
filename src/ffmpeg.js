4f29763495d1def93842a5abd737253bf7541be7
KM <the.real.laplace@gmail.com>
2024-02-28 02:03:30 +0000

    code

---
+++ src/ffmpeg.js
@@ -0,0 +1,58 @@
+import ffmpeg from 'ffmpeg-static'
+import { spawn } from 'child_process'
+import path from 'path'
+
+export const loadImage = async (filepath, options) => (await loadVideo(filepath, options))[0]
+
+export const loadVideo = async (filepath, { width, height, framerate }) => {
+	const proc = spawn(
+		ffmpeg,
+		[
+			[ '-i', path.relative(process.cwd(), filepath).replaceAll(path.sep, path.posix.sep) ],
+			'-filter:v',
+			[
+				framerate && `fps=fps=${framerate}`,
+				`scale=${width}:${height}`,
+				'format=pix_fmts=rgb24',
+			].filter(Boolean).join(','),
+			[ '-f', 'rawvideo' ],
+			'-',
+		].flat(),
+	)
+
+	const frameSize = width * height * 3
+	const frames = []
+	let chunks = []
+	let size = 0
+
+	const append = (data) => {
+		const end = frameSize - size
+		const chunk = data.slice(0, end)
+		chunks.push(chunk)
+		size += chunk.length
+
+		if (size === frameSize) {
+			frames.push(Buffer.concat(chunks))
+			chunks = []
+			size = 0
+			append(data.slice(end))
+		}
+	}
+
+	proc.stdout.on('data', append)
+
+	const stderrChunks = []
+	proc.stderr.on('data', (chunk) => {
+		stderrChunks.push(chunk.toString())
+	})
+
+	return await new Promise((resolve, reject) => {
+		proc.on('close', (code) => {
+			code
+				? reject(Object.assign(new Error(`exit code ${code}`), {
+					stderr: stderrChunks.join(''),
+				}))
+				: resolve(frames)
+		})
+	})
+}
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import sdl from '@kmamal/sdl'
+import path from 'path'
+import url from 'url'
+import { setTimeout } from 'timers/promises'
+import { loadImage, loadVideo } from './ffmpeg.js'
+
+const framerate = 25
+
+const window = sdl.video.createWindow()
+const { pixelWidth: width, pixelHeight: height } = window
+
+const dir = path.dirname(url.fileURLToPath(import.meta.url))
+const [ image, video ] = await Promise.all([
+	loadImage(path.join(dir, '../assets/image.png'), { width, height }),
+	loadVideo(path.join(dir, '../assets/video.mp4'), { width, height, framerate }),
+])
+
+let startTime = null
+let lastIndex = null
+
+window.on('mouseButtonUp', () => {
+	startTime = startTime ? null : Date.now()
+	render()
+})
+
+const render = async () => {
+	while (!window.destroyed) {
+		// Render pause image
+		if (!startTime) {
+			window.render(width, height, width * 3, 'rgb24', image)
+			return
+		}
+
+		// Render video
+		{
+			const time = (Date.now() - startTime) / 1e3
+			const index = Math.floor(time * framerate)
+
+			if (index === lastIndex) { continue }
+
+			if (index >= video.length) {
+				startTime = null
+				lastIndex = null
+
+				window.render(width, height, width * 3, 'rgb24', image)
+				return
+			}
+
+			window.render(width, height, width * 3, 'rgb24', video[index])
+			lastIndex = index
+		}
+
+		await setTimeout(0)
+	}
+}
+
+render()
